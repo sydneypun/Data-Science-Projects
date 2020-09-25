c3cb79c3e019e71099096b53291e966c42922dd0
Sydney Pun <spun001@ucr.edu>
2020-09-24 17:19:48 -0700

    Adding Apple to the simple stock analysis.

--- Simple Stock Analysis/stocks.py
+++ Simple Stock Analysis/stocks.py
@@ -28,7 +28,7 @@ st.line_chart(ticker_df.Volume)
 # ticker symbol is defined here
 ticker_symbol1 = 'AMZN'
 # get data on this ticker
-ticker_data1 = yf.Ticker(ticker_symbol3)
+ticker_data1 = yf.Ticker(ticker_symbol1)
 # get the historical prices for this ticker
 ticker_df1 = ticker_data1.history(period='1d', start='2010-9-20', end='2020-9-20')
 
@@ -38,29 +38,43 @@ st.write(""" ## Amazon Stock Volume """)
 st.line_chart(ticker_df1.Volume)
 
 
-# NETFLIX STOCK
+# APPLE STOCK
 # ticker symbol is defined here
-ticker_symbol2 = 'NFLX'
+ticker_symbol2 = 'AAPL'
 # get data on this ticker
-ticker_data2 = yf.Ticker(ticker_symbol4)
+ticker_data2 = yf.Ticker(ticker_symbol2)
 # get the historical prices for this ticker
 ticker_df2 = ticker_data2.history(period='1d', start='2010-9-20', end='2020-9-20')
 
-st.write(""" ## Netflix Stock Closing Price """)
+st.write(""" ## Apple Stock Closing Price """)
 st.line_chart(ticker_df2.Close)
-st.write(""" ## Netflix Stock Volume """)
+st.write(""" ## Apple Stock Volume """)
 st.line_chart(ticker_df2.Volume)
 
 
-# GOOGLE STOCK
+# NETFLIX STOCK
 # ticker symbol is defined here
-ticker_symbol3 = 'GOOGL'
+ticker_symbol3 = 'NFLX'
 # get data on this ticker
 ticker_data3 = yf.Ticker(ticker_symbol3)
 # get the historical prices for this ticker
 ticker_df3 = ticker_data3.history(period='1d', start='2010-9-20', end='2020-9-20')
 
-st.write(""" ## Google Stock Closing Price """)
+st.write(""" ## Netflix Stock Closing Price """)
 st.line_chart(ticker_df3.Close)
-st.write(""" ## Google Stock Volume """)
+st.write(""" ## Netflix Stock Volume """)
 st.line_chart(ticker_df3.Volume)
+
+
+# GOOGLE STOCK
+# ticker symbol is defined here
+ticker_symbol4 = 'GOOGL'
+# get data on this ticker
+ticker_data4 = yf.Ticker(ticker_symbol4)
+# get the historical prices for this ticker
+ticker_df4 = ticker_data4.history(period='1d', start='2010-9-20', end='2020-9-20')
+
+st.write(""" ## Google Stock Closing Price """)
+st.line_chart(ticker_df4.Close)
+st.write(""" ## Google Stock Volume """)
+st.line_chart(ticker_df4.Volume)
