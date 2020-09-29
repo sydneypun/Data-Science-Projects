d5b8b903814282e6b25fda77c942a6fb72e61ef4
Sydney Pun <spun001@ucr.edu>
2020-09-29 14:54:28 -0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -9,7 +9,7 @@ from PIL import Image
 st.write(""" 
 # Stock Market Dashboard
 This is a visual representation of the FAANG companies' stocks and Tesla's stock. The date range is from 
-October 1, 2010 - October 1, 2020. Date is subject to change. 
+January 1, 2010 - October 1, 2020. Date is subject to change. 
 """)
 
 image = Image.open("Images\stonks.jpg")
@@ -21,7 +21,7 @@ st.sidebar.header('User Input')
 # Create a function to get the user's input
 def get_input(): 
     start_date = st.sidebar.text_input("Start Date", "2010-01-01")
-    end_date = st.sidebar.text_input("End Date", "2020-01-01")
+    end_date = st.sidebar.text_input("End Date", "2020-10-01")
     stock_symbol = st.sidebar.text_input("Stock Symbol", "AMZN")
     return start_date, end_date, stock_symbol
 
@@ -43,6 +43,61 @@ def get_company_name():
         'None'
 
 # Create a function to get the proper company data and time frame inputted by the user
-#def get_data(symbol, start, end): 
+def get_data(symbol, start, end): 
     # Load the data
+    if symbol.upper() == 'AMZN': 
+        df = pd.read_csv("Datasets/Amazon.csv")
+    elif symbol.upper() == 'AAPL': 
+        df = pd.read_csv("Datasetes/Apple.csv")
+    elif symbol.upper() == 'FB': 
+        df = pd.read_csv("Datasets/Facebook.csv")
+    elif symbol.upper() == 'GOOG':
+        df = pd.read_csv("Datasets/Google.csv")
+    elif symbol.upper() == 'NFLX': 
+        df = pd.read_csv("Datasets/Netflix.csv")
+    else: 
+        df = pd.DataFrame(columns = ['Date', 'Close','Open', 'Volume', 'Adj Close', 'High', 'Low'])
 
+    # Get the date range
+    start = pd.to_datetime(start)
+    end = pd.to_datetime(end)
+
+    # Set the start and end index rows both to 0
+    start_row = 0
+    end_row = 0
+
+    # Start the date from the top of the data set and go down to see if there's a start date
+    for i in range(0, len(df)): 
+        if start <= pd.datetime(df['Date'][i]): 
+            start_row = i 
+            break 
+
+    # Start the date from the bottom of the data set and go up
+    for j in range(0, len(df)): 
+        if end >= pd.to_datetime(df['Date'][len(df)-1-j]):
+            end_row = len(df) -1 - j
+            break 
+    
+    # Set the index to be the date
+    df = df.set_index(pd.DatetimeIndex(df['Date'].values))
+
+    return df.iloc[start_row:end_row + 1, :]
+
+# Get the user's input
+start, end, symbol = get_input()
+# Get the data
+df = get_data(symbol, start, end)
+# Get the company name
+company_name = get_company_name(symbol.upper())
+
+# Display the close price
+st.header(company_name + "Close Price\n")
+st.line_chart(df['Close'])
+
+# Display the volume
+st.header(company_name + "Volume\n")
+st.line_chart(df['Volume'])
+
+# Get statistics on the data
+st.header('Data Statistics')
+st.write(df.describe())
